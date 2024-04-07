d3dd1d2ee555d2cb4c930b1e638c4a1afc8bd698
Arturo Gutierrez Loza <arturogl@mac.com>
2024-04-07 17:20:58 -0600

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -132,7 +132,7 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(data["name"], account.name)    
+        self.assertEqual(data["name"], account.name)
 
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
@@ -145,7 +145,7 @@ class TestAccountService(TestCase):
         resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(len(data), 5)   
+        self.assertEqual(len(data), 5)
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -171,4 +171,4 @@ class TestAccountService(TestCase):
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
-        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)                             
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
